13fa8294966a3fd32fd8c1991efed84b15403bae
Jason <jason.dixon.email@gmail.com>
2015-08-13 15:45:23 +1200

    added legs

--- baseRig.py
+++ baseRig.py
@@ -1,2 +1,31 @@
 # The base rig structure
 from os.path import join
+
+rig = {
+    "root" : {
+        "pelvis" : {
+            "left_hip" : {
+                "left_knee" : {
+                    "left_ankle" : {
+                        "left_heel" : {
+                            "left_foot_ball" : {
+                                "left_foot_toe" : {}
+                            }
+                        }
+                    }
+                }
+            },
+            "right_hip" : {
+                "right_knee" : {
+                    "right_ankle" : {
+                        "right_heel" : {
+                            "right_foot_ball" : {
+                                "right_foot_toe" : {}
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
