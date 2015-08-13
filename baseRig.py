412ee5d325a073fa2ff5cc3e0dbb576fab0f8a90
Jason <jason.dixon.email@gmail.com>
2015-08-13 15:59:05 +1200

    added spine

--- baseRig.py
+++ baseRig.py
@@ -3,24 +3,33 @@ from os.path import join
 
 rig = {
     "root" : {
-        "pelvis" : {
-            "left_hip" : {
-                "left_knee" : {
-                    "left_ankle" : {
-                        "left_heel" : {
-                            "left_foot_ball" : {
-                                "left_foot_toe" : {}
+        "spine_pelvis" : {
+            "spine_middle" : {
+                "spine_lower_ribcage" : {
+                    "spine_neck_base" : {
+                        "spine_skull_base" : {
+                            "spine_skull_top" : {}
+                        }
+                    }
+                }
+            }
+            "left_leg_hip" : {
+                "left_leg_knee" : {
+                    "left_leg_ankle" : {
+                        "left_leg_foot_heel" : {
+                            "left_leg_foot_ball" : {
+                                "left_leg_foot_toe" : {}
                             }
                         }
                     }
                 }
-            },
-            "right_hip" : {
-                "right_knee" : {
-                    "right_ankle" : {
-                        "right_heel" : {
-                            "right_foot_ball" : {
-                                "right_foot_toe" : {}
+            }
+            "right_leg_hip" : {
+                "right_leg_knee" : {
+                    "right_leg_ankle" : {
+                        "right_leg_foot_heel" : {
+                            "right_leg_foot_ball" : {
+                                "right_leg_foot_toe" : {}
                             }
                         }
                     }
