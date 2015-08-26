f63779ef681b1262a1f12d3440bcb0174a7d6dcf
Internetimagery <jason.dixon.email@gmail.com>
2015-08-27 08:47:18 +1200

    no more mesh option

--- exportRig.py
+++ exportRig.py
@@ -1,7 +1,7 @@
 # Export rig by baking out curves first
 import maya.cmds as cmds
 import maya.mel as mel
-from os.path import join, exists
+from os.path import join, exists, realpath
 # from makeRig import NameSpace, GetRoot
 
 def NameSpace(name, prefix=None):
@@ -21,46 +21,17 @@ class ExportRig (object):
         s.win = cmds.window(rtf=True, w=300, t="Export Animation")
         cmds.columnLayout(adj=True)
         s.prefix = cmds.textFieldGrp(l="(optional) Prefix: ")
-        s.meshRow = cmds.rowLayout(nc=2, adj=2)
-        cmds.checkBoxGrp(l="Include Mesh in Export", cc=s.meshTextActive)
-        s.mesh = cmds.textField(en=False, pht="Mesh Name: ", cc=lambda x: s.meshTextValidate(s.mesh, x))
-        cmds.setParent("..")
+        s.animOnly = cmds.checkBoxGrp(l="Export Animation Only? ")
         s.charName = cmds.textFieldGrp(l="Character Name: ", cc=lambda x: s.validateFilename(s.charName, x))
         s.animName = cmds.textFieldGrp(l="Animation Name: ", cc=lambda x: s.validateFilename(s.animName, x))
         s.fileName = cmds.textFieldButtonGrp(ed=False, l="Save Folder: ", bl="Open", bc=s.validateDirName)
         s.exportBtn = cmds.button(l="Export Animation", h=80, c=s.export, en=True)
         cmds.showWindow(s.win)
         s.valid = {
-            s.mesh : True,
             s.charName : False,
             s.animName : False,
             s.fileName : False
         }
-
-    """
-    Activate Mesh textfield
-    """
-    def meshTextActive(s, state):
-        cmds.textField(s.mesh, e=True, en=state)
-        text = cmds.textField(s.mesh, q=True, tx=True).strip()
-        if state and text:
-            s.meshTextValidate(text)
-
-    """
-    Validate Mesh textfield
-    """
-    def meshTextValidate(s, control, text):
-        text = text.strip()
-        if cmds.objExists(text):
-            s.valid[control] = True
-            cmds.rowLayout(s.meshRow, e=True, bgc=(0.3,1,0.3))
-            cmds.textField(s.mesh, e=True, bgc=(0.3,1,0.3))
-            s.activateExportButton()
-        else:
-            s.valid[control] = False
-            cmds.textField(s.mesh, e=True, bgc=(1,0.4,0.4))
-            cmds.rowLayout(s.meshRow, e=True, bgc=(1,0.4,0.4))
-            s.activateExportButton()
 
     """
     Validate Character / Animation filename
@@ -108,12 +79,12 @@ class ExportRig (object):
     Export the rig animation!
     """
     def export(s, *junk):
-        filePath = join(
+        filePath = realpath(join(
             cmds.textFieldButtonGrp(s.fileName, q=True, tx=True),
             "%s@%s.fbx" % (
                 cmds.textFieldGrp(s.charName, q=True, tx=True),
                 cmds.textFieldGrp(s.animName, q=True, tx=True))
-        )
+        ))
         if not exists(filePath) or "Yes" == cmds.confirmDialog(
                                             t="Just a moment...",
                                             m="The file currently exists. Override?",
@@ -122,64 +93,89 @@ class ExportRig (object):
                                             cancelButton="No",
                                             dismissString="No"):
             prefix = cmds.textFieldGrp(s.prefix, q=True, tx=True).strip()
-            origSelection = cmds.ls(sl=True) # Store original selection to return to
-            # skeleton = s.locateSkeleton()
-            skeleton = cmds.ls(sl=True)
-
-
-            with Undo():
-                # Bake out the rig animation
-                cmds.bakeResults(skeleton,
-                    t=(0,20),
-                    sm=True,
-                    dic=True,
-                    sac=True,
-                    ral=True,
-                    mr=True,
-                    sr=(True, 5)
-                    )
-                # Clean up extra channels that are not used
-                cmds.delete(skeleton, sc=True)
-                # Export the FBX file
-                commands = "FBXResetExport; FBXExportInAscii -v true;"
-                if not cmds.textField(s.mesh, q=True, en=True) and s.valid[s.mesh]:
-                    commands += "FBXExportAnimationOnly -v true;"
-                commands += """
+            baseName = NameSpace(GetRoot(), prefix)
+            baseObj = cmds.ls(baseName, r=True)
+            if baseObj:
+                cmds.select(baseObj)
+                commands =  """
+FBXResetExport; FBXExportInAscii -v true;
+FBXExportCameras -v false; FBXExportLights -v false;
+FBXExportUpAxis %s; FBXExportUseSceneName -v false;
+FBXExportGenerateLog -v false; FBXExportConstraints -v false;
+FBXExportAxisConversionMethod addFbxRoot;
+FBXExportApplyConstantKeyReducer -v true;
+""" % cmds.upAxis(q=True, ax=True)
+                if cmds.checkBoxGrp(s.animOnly, q=True, v1=True):
+                    commands += """
+FBXExportAnimationOnly -v true;
+"""
+                else:
+                    commands += """
 FBXExportSkins -v true;
 FBXExportShapes -v true;
 FBXExportInputConnections -v true;
-FBXExportBakeComplexAnimation -v false;
-FBXExportCameras -v false;
-FBXExportConstraints -v false;
 FBXExportEmbeddedTextures -v false;
-FBXExportGenerateLog -v false;
-FBXExportLights -v false;
-//FBXExportSplitAnimationIntoTakes "takename" 1 24;
-FBXExportUpAxis %(upaxis)s;
-FBXExportUseSceneName -v false;
-FBXExport -f \"%(file)s\" -s;
-""" % { "file"  : filePath, "upaxis": cmds.upAxis(q=True, ax=True) }
+FBXExportSmoothMesh -v false;
+FBXExportSmoothingGroups -v true;
+FBXExportTangents - true;
+"""
+                commands += "FBXExport -f \"%s\" -s;" % filePath
                 mel.eval(commands)
+            else:
+                cmds.confirmDialog(t="Bugger...", m="Couldn't find a matching rig.")
 
+                #
+                # # largeList = cmds.listRelatives(baseObj, ad=True, pa=True, ni=True)
+                # cmds.select(baseObj, r=True)
+                # cmds.select(baseObj, hi=True, tgl=True)
+                # joints = cmds.ls(sl=True, typ="joint")
+                # cmds.select()
+                # if joints:
+                #     return joints
+#
 
+#
+#              skeleton = s.locateSkeleton()
+#             skeleton = cmds.ls(sl=True)
+#
+#
+#             with Undo():
+#                 # Bake out the rig animation
+#                 cmds.bakeResults(skeleton,
+#                     t=(0,20),
+#                     sm=True,
+#                     dic=True,
+#                     sac=True,
+#                     ral=True,
+#                     mr=True,
+#                     sr=(True, 5)
+#                     )
+#                 # Clean up extra channels that are not used
+#                 cmds.delete(skeleton, sc=True)
+#                 # Export the FBX file
+#                 commands = "FBXResetExport; FBXExportInAscii -v true;"
+#                 if cmds.textField(s.mesh, q=True, en=True) and s.valid[s.mesh]:
+#                     skeleton.append(cmds.textField(s.mesh, q=True, tx=True))
+#                     print skeleton
+#                 if not cmds.textField(s.mesh, q=True, en=True) and s.valid[s.mesh]:
+#                     commands += "FBXExportAnimationOnly -v true;"
+#                 commands += """
+# FBXExportSkins -v true;
+# FBXExportShapes -v true;
+# FBXExportInputConnections -v true;
+# FBXExportBakeComplexAnimation -v false;
+# FBXExportCameras -v false;
+# FBXExportConstraints -v false;
+# FBXExportEmbeddedTextures -v false;
+# FBXExportGenerateLog -v false;
+# FBXExportLights -v false;
+# //FBXExportSplitAnimationIntoTakes "takename" 1 24;
+# FBXExportUpAxis %(upaxis)s;
+# FBXExportUseSceneName -v false;
+# FBXExport -f \"%(file)s\" -s;
+# """ % { "file"  : filePath, "upaxis": cmds.upAxis(q=True, ax=True) }
+#                 mel.eval(commands)
 
-
-
-    """
-    Select the skeleton
-    """
-    def locateSkeleton(s, prefix=None):
-        baseName = NameSpace(GetRoot(), prefix)
-        baseObj = cmds.ls(baseName, r=True)
-        if baseObj:
-            # largeList = cmds.listRelatives(baseObj, ad=True, pa=True, ni=True)
-            cmds.select(baseObj, r=True)
-            cmds.select(baseObj, hi=True, tgl=True)
-            joints = cmds.ls(sl=True, typ="joint")
-            if joints:
-                return joints
-        cmds.confirmDialog(t="Bugger...", m="Couldn't find a matching rig.")
-        return None
 
 """
 Keep undos tidy
@@ -187,8 +183,11 @@ Keep undos tidy
 class Undo(object):
     def __enter__(s):
         cmds.undoInfo(ock=True)
+        s.selection = cmds.ls(sl=True) # Store original selection to return to
+
     def __exit__(s, err, type, trace):
         cmds.undoInfo(cck=True)
         cmds.undo()
+        cmds.select(s.selection, r=True)
 
 ExportRig()
