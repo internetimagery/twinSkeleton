dd69ae4cf671ea42e1b6d3c656439ee9f80a4ef2
Internetimagery <jason.dixon.email@gmail.com>
2015-08-27 03:28:35 +1200

    added file path option

--- exportRig.py
+++ exportRig.py
@@ -22,14 +22,22 @@ class ExportRig (object):
         s.win = cmds.window(rtf=True, w=300, t="Export Animation")
         cmds.columnLayout(adj=True)
         s.prefix = cmds.textFieldGrp(l="(optional) Prefix: ")
-        cmds.rowLayout(nc=2, adj=2)
+        s.meshRow = cmds.rowLayout(nc=2, adj=2)
         cmds.checkBoxGrp(l="Include Mesh in Export", cc=s.meshTextActive)
-        s.mesh = cmds.textField(en=False, pht="Mesh Name: ", cc=s.meshTextValidate)
+        s.mesh = cmds.textField(en=False, pht="Mesh Name: ", cc=lambda x: s.meshTextValidate(s.mesh, x))
         cmds.setParent("..")
-        s.charName = cmds.textFieldGrp(l="Character Name: ")
-        s.animName = cmds.textFieldGrp(l="Animation Name: ")
-        # cmds.button(l="Load Template and Build Rig", h=100, c=s.export)
+        s.charName = cmds.textFieldGrp(l="Character Name: ", cc=lambda x: s.validateFilename(s.charName, x))
+        s.animName = cmds.textFieldGrp(l="Animation Name: ", cc=lambda x: s.validateFilename(s.animName, x))
+        s.fileName = cmds.textFieldButtonGrp(ed=False, l="Save Folder: ", bl="Open", bc=s.validateDirName)
+        s.animName = cmds.textFieldGrp(l="Animation Name: ", cc=lambda x: s.validateFilename(s.animName, x))
+        cmds.button(l="Export Animation", h=80, c=s.export)
         cmds.showWindow(s.win)
+        s.valid = {
+            s.mesh : True,
+            s.charName : False,
+            s.animName : False,
+            s.fileName : False
+        }
 
     """
     Activate Mesh textfield
@@ -43,20 +51,54 @@ class ExportRig (object):
     """
     Validate Mesh textfield
     """
-    def meshTextValidate(s, text):
+    def meshTextValidate(s, control, text):
         text = text.strip()
         if cmds.objExists(text):
+            s.valid[control] = True
+            cmds.rowLayout(s.meshRow, e=True, bgc=(0.3,1,0.3))
             cmds.textField(s.mesh, e=True, bgc=(0.3,1,0.3))
         else:
+            s.valid[control] = False
             cmds.textField(s.mesh, e=True, bgc=(1,0.4,0.4))
+            cmds.rowLayout(s.meshRow, e=True, bgc=(1,0.4,0.4))
+
+    """
+    Validate Character / Animation filename
+    """
+    def validateFilename(s, control, text):
+        if 120 < len(text):
+            s.valid[control] = False
+            return cmds.textFieldGrp(control, e=True, bgc=(1,0.4,0.4))
+        for invalid in ["*", "|", "\\", "/", ":", "\"", "<", ">", "?"]:
+            if invalid in text:
+                s.valid[control] = False
+                return cmds.textFieldGrp(control, e=True, bgc=(1,0.4,0.4))
+        s.valid[control] = True
+        cmds.textFieldGrp(control, e=True, bgc=(0.3,1,0.3))
+
+    """
+    Load file name for saving
+    """
+    def validateDirName(s, *path):
+        path = cmds.fileDialog2(fm=3) # Save file
+        if path:
+            cmds.textFieldGrp(s.fileName, e=True, tx=path[0])
+        if cmds.textFieldGrp(s.fileName, q=True, tx=True):
+            s.valid[s.fileName] = True
+            cmds.textFieldGrp(s.fileName, e=True, bgc=(0.3,1,0.3))
+        else:
+            s.valid[s.fileName] = False
+            cmds.textFieldButtonGrp(s.fileName, e=True, bgc=(1,0.4,0.4))
 
     """
     Export the rig animation!
     """
     def export(s, *junk):
-        prefix = cmds.textField(s.prefix, q=True, tx=True).strip()
-        origSelection = cmds.ls(sl=True) # Store original selection to return to
-        skeleton = s.locateSkeleton()
+        path = cmds.fileDialog2(dialogStyle=2, fm=3) # Save file
+
+        # prefix = cmds.textField(s.prefix, q=True, tx=True).strip()
+        # origSelection = cmds.ls(sl=True) # Store original selection to return to
+        # skeleton = s.locateSkeleton()
 
     """
     Select the skeleton
